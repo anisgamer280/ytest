118c24add088e536e01ce3c5ee0dd151e9671979
anisgamer280 <104579425+anisgamer280@users.noreply.github.com>
2022-04-28 18:12:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,13 +9,41 @@ const client7 = new Discord.Client();
 const client8 = new Discord.Client();
 const client9 = new Discord.Client();
 const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+const client12 = new Discord.Client();
+const client13 = new Discord.Client();
+const client14 = new Discord.Client();
+const client15 = new Discord.Client();
+const client16 = new Discord.Client();
+const client17 = new Discord.Client();
+const client18 = new Discord.Client();
+const client19 = new Discord.Client();
+const client20 = new Discord.Client();
+const client21 = new Discord.Client();
+const client22 = new Discord.Client();
+const client23 = new Discord.Client();
+const client24 = new Discord.Client();
+const client25 = new Discord.Client();
+const client26 = new Discord.Client();
+const client27 = new Discord.Client();
+const client28 = new Discord.Client();
+const client29 = new Discord.Client();
+const client30 = new Discord.Client();
+const client31 = new Discord.Client();
+const client32 = new Discord.Client();
+const client33 = new Discord.Client();
+const client34 = new Discord.Client();
+const client35 = new Discord.Client();
+const client36 = new Discord.Client();
+const client37 = new Discord.Client();
+const client38 = new Discord.Client();
+const client39 = new Discord.Client();
+const client40 = new Discord.Client();
 
 
 var prefix = "s";
 
-const ownerID = '608267144878096404';
-
-
+const ownerID = '630440873473081374';
 
 
 
@@ -32,3 +60,33 @@ client7.login(process.env.t7);//
 client8.login(process.env.t8);// 
 client9.login(process.env.t9);// 
 client10.login(process.env.t10);// 
+client11.login(process.env.t11);// 
+client12.login(process.env.t12);// 
+client13.login(process.env.t13);// 
+client14.login(process.env.t14);// 
+client15.login(process.env.t15);// 
+client16.login(process.env.t16);// 
+client17.login(process.env.t17);// 
+client18.login(process.env.t18);// 
+client19.login(process.env.t19);// 
+client20.login(process.env.t20);// 
+client21.login(process.env.t21);// 
+client22.login(process.env.t22);// 
+client23.login(process.env.t23);// 
+client24.login(process.env.t24);// 
+client25.login(process.env.t25);// 
+client26.login(process.env.t26);// 
+client27.login(process.env.t27);// 
+client28.login(process.env.t28);// 
+client29.login(process.env.t29);// 
+client30.login(process.env.t30);// 
+client31.login(process.env.t31);// 
+client32.login(process.env.t32);// 
+client33.login(process.env.t33);// 
+client34.login(process.env.t34);// 
+client35.login(process.env.t35);// 
+client36.login(process.env.t36);// 
+client37.login(process.env.t37);// 
+client38.login(process.env.t38);// 
+client39.login(process.env.t39);// 
+client40.login(process.env.t40);// 
