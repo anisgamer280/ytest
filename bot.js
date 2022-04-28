374f26bcf16a863cf3fa2089dd48bc5876268135
anisgamer280 <104579425+anisgamer280@users.noreply.github.com>
2022-04-28 19:02:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,36 +1,109 @@
-const Eris = require("eris");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+const client12 = new Discord.Client();
+const client13 = new Discord.Client();
+const client14 = new Discord.Client();
+const client15 = new Discord.Client();
+const client16 = new Discord.Client();
+const client17 = new Discord.Client();
+const client18 = new Discord.Client();
+const client19 = new Discord.Client();
+const client20 = new Discord.Client();
+const client21 = new Discord.Client();
+const client22 = new Discord.Client();
+const client23 = new Discord.Client();
+const client24 = new Discord.Client();
+const client25 = new Discord.Client();
+const client26 = new Discord.Client();
+const client27 = new Discord.Client();
+const client28 = new Discord.Client();
+const client29 = new Discord.Client();
+const client30 = new Discord.Client();
+const client31 = new Discord.Client();
+const client32 = new Discord.Client();
+const client33 = new Discord.Client();
+const client34 = new Discord.Client();
+const client35 = new Discord.Client();
+const client36 = new Discord.Client();
+const client37 = new Discord.Client();
+const client38 = new Discord.Client();
+const client39 = new Discord.Client();
+const client40 = new Discord.Client();
 
 
-const bot = new Eris(process.env.t1);
-const bot2 = new Eris(process.env.t2);
-const bot3 = new Eris(process.env.t3);
-const bot4 = new Eris(process.env.t4);
-const bot5 = new Eris(process.env.t5);
-const bot6 = new Eris(process.env.t6);
-const bot7 = new Eris(process.env.t7);
-const bot8 = new Eris(process.env.t8);
-const bot9 = new Eris(process.env.t9);
-const bot10 = new Eris(process.env.t10);
+var prefix = "s";
 
-bot.connect()
-bot2.connect()
-bot3.connect()
-bot4.connect()
-bot5.connect()
-bot6.connect()
-bot7.connect()
-bot8.connect()
-bot9.connect()
-bot10.connect()
+const ownerID = '630440873473081374';
 
 
-bot.editStatus("dnd", { name: "Far Cry 3", type: 4 });
-bot2.editStatus("dnd", { name: "FLENN", type: 4 });
-bot3.editStatus("dnd", { name: "YOTUBE", type: 4 });
-bot4.editStatus("dnd", { name: "ForzaHorizon 5", type: 4 });
-bot5.editStatus("dnd", { name: "Spotify", type: 2 });
-bot6.editStatus("dnd", { name: "YOTUBE", type: 3 });
-bot7.editStatus("dnd", { name: "Gta V", type: 0 });
-bot8.editStatus("dnd", { name: "Luve yo", type: 2 });
-bot9.editStatus("dnd", { name: "La casa de papel", type: 3 });
-bot10.editStatus("dnd", { name: "Far Cry  2", type: 0 });
+
+
+
+
+client.login(process.env.t1);// 
+client2.login(process.env.t2);// 
+client3.login(process.env.t3);// 
+client4.login(process.env.t4);// 
+client5.login(process.env.t5);// 
+client6.login(process.env.t6);// 
+client7.login(process.env.t7);// 
+client8.login(process.env.t8);// 
+client9.login(process.env.t9);// 
+client10.login(process.env.t10);// 
+client11.login(process.env.t11);// 
+client12.login(process.env.t12);// 
+client13.login(process.env.t13);// 
+client14.login(process.env.t14);// 
+client15.login(process.env.t15);// 
+client16.login(process.env.t16);// 
+client17.login(process.env.t17);// 
+client18.login(process.env.t18);// 
+client19.login(process.env.t19);// 
+client20.login(process.env.t20);// 
+client21.login(process.env.t21);// 
+client22.login(process.env.t22);// 
+client23.login(process.env.t23);// 
+client24.login(process.env.t24);// 
+client25.login(process.env.t25);// 
+client26.login(process.env.t26);// 
+client27.login(process.env.t27);// 
+client28.login(process.env.t28);// 
+client29.login(process.env.t29);// 
+client30.login(process.env.t30);// 
+client31.login(process.env.t31);// 
+client32.login(process.env.t32);// 
+client33.login(process.env.t33);// 
+client34.login(process.env.t34);// 
+client35.login(process.env.t35);// 
+client36.login(process.env.t36);// 
+client37.login(process.env.t37);// 
+client38.login(process.env.t38);// 
+client39.login(process.env.t39);// 
+client40.login(process.env.t40);// 
+
+client.on("ready", () => {
+    client.user.setActivity("YT Abofalah", { type: "LISTENING"})
+})
+
+client2.on("ready", () => {
+    client2.user.setActivity("Netflix Lucifer EP1  S02", { type: "WATCHING"})
+})
+
+
+client3.on("ready", () => {
+    client3.user.setActivity("Spotifey", { type: "PLAYING"})
+})
+
+client4.on("ready", () => {
+    client4.user.setActivity("Spotifey", { type: "PLAYING"})
+})
